7859c37c7582d68bb1007e1b70fb97df3a060461
Anjam Alam <alamanjam@hotmail.com>
2017-04-16 14:34:26 -0400

    trying to fix white carousel images

--- pubmed.py
+++ pubmed.py
@@ -9,15 +9,17 @@ from firebase import firebase
 from nltk.corpus import stopwords
 
 fb = firebase.FirebaseApplication("https://trendinginmedicine-f41fc.firebaseio.com/")
-
+journals = set()
 keyWords = dict()
 translator = str.maketrans('', '', string.punctuation)
-commonWords = stopwords.words('english') + ['review', "surgery", "report", "peer", "patients", "systematic", "surgical", "management", "meta-analysis", "versus", "total", "factors", "controlled", "repair", "cancer", "randomized", "study", "outcomes", "reconstruction", "safety", "open", "analysis", 'metaanalysis', '“comparison', '“the', 'study”', "cancer:", "clinical", "case", "oral", "treatment", "chapter", "—", "using", "cell", "cases", "head", "evaluation", "literature", "neck", "patient", "technique", "retrospective", "flap", "facial", "experience", "use", "following", "approach", "comparison", "syndrome", "threedimensional", "disease", "bone", "joint", "implants", "postoperative", "postoperative", "rare", "effect", "outcome", "dental", "artery", "carcinoma","risk", "associated", 'therapy', 'acute', 'lower', "maxillary", 'resection', 'bypass', "vascular", "longterm", "graft", "cohort", "transplantation", "preoperative", "prospective", "injury", "mortality", "care", "extremity", "discussion", "trial", "primary", "impact", "quality", "complications", "surgeons", "undergoing", "intraoperative", "research", "volume", "hospital", "survival", "improves", "role", "results", "prognosis", "prognostic", "control", "lung", "function", "centers", "center", "multicenter", "heart", "association", "coronary", "cardiovascular", "failure", "american", "valve", "atrial", "cardiac", "left", "pulmonary", "fibrillation", "pressure", "perceutaneous", "aortic", "infaraction", "blood", "chronic", "college", "stroke", "reply", "events", "guidelines", "women", "mitral", "data", "trials"]
+commonWords = stopwords.words('english') + ['review', "surgery", "report", "peer", "patients", "systematic", "surgical", "management", "meta-analysis", "versus", "total", "factors", "controlled", "repair", "cancer", "randomized", "study", "outcomes", "reconstruction", "safety", "open", "analysis", 'metaanalysis', '“comparison', '“the', 'study”', "cancer:", "clinical", "case", "oral", "treatment", "chapter", "—", "using", "cell", "cases", "head", "evaluation", "literature", "neck", "patient", "technique", "retrospective", "flap", "facial", "experience", "use", "following", "approach", "comparison", "syndrome", "threedimensional", "disease", "bone", "joint", "implants", "postoperative", "postoperative", "rare", "effect", "outcome", "dental", "artery", "carcinoma","risk", "associated", 'therapy', 'acute', 'lower', "maxillary", 'resection', 'bypass', "vascular", "longterm", "graft", "cohort", "transplantation", "preoperative", "prospective", "injury", "mortality", "care", "extremity", "discussion", "trial", "primary", "impact", "quality", "complications", "surgeons", "undergoing", "intraoperative", "research", "volume", "hospital", "survival", "improves", "role", "results", "prognosis", "prognostic", "control", "lung", "function", "centers", "center", "multicenter", "heart", "association", "coronary", "cardiovascular", "failure", "american", "valve", "atrial", "cardiac", "left", "pulmonary", "fibrillation", "pressure", "perceutaneous", "aortic", "infaraction", "blood", "chronic", "college", "stroke", "reply", "events", "guidelines", "women", "mitral", "data", "trials", "laparoscopic", "breast", "general", "model", "early", "invasive", "novel", "value", "time", "training", "score", "medical", "single", "portal", "development", "years", "emergency", "recovery"]
 idlist = []
 titles = []
 s = "+"
-l = ["Journal of the American College of Cardiology[ta]", "JACC. Heart failure[ta]", "JACC. Cardiovascular interventions[ta]", "Chest[ta]", "American heart journal[ta]", "Journal of the American Heart Association[ta]", "\"European heart journal\"[ta]"]
-#l = ["Journal of the American College of Surgeons[ta]", "\"Surgery\"[ta]", "International Journal of Surgery[ta]", "The European journal of surgery[ta]", "The British journal of surgery[ta]", "Surgery today[ta]", "Annals of Surgery[ta]"] #"British Journal of Oral and Maxillofacial Surgery[ta]", "The Journal of Heart and Lung Transplantation[ta]", "Annals of Vascular Surgery[ta]"]
+topic = 'surgery'
+#l = ["Journal of the American College of Cardiology[ta]", "JACC. Heart failure[ta]", "JACC. Cardiovascular interventions[ta]", "Chest[ta]", "American heart journal[ta]", "Journal of the American Heart Association[ta]", "\"European heart journal\"[ta]"]
+l = ["JAMA surgery[ta]", "World journal of surgery[ta]", "American journal of surgery[ta]", "The Surgical clinics of North America[ta]", "The Journal of surgical research[ta]", "Journal of surgical education[ta]", "Adv Surg[ta]", "European surgical research[ta]", "\"The Journal of the International College of Surgeons\"[ta]", "Journal of the American College of Surgeons[ta]", "\"Bulletin of the American College of Surgeons\"[ta]", "\"Surgery\"[ta]", "International journal of surgery[ta]", "\"The European journal of surgery\"[ta]", "Surgery today[ta]", "Annals of surgery[ta]", "The British journal of surgery[ta]", "The American surgeon[ta]", "\"International journal of surgery and research\"[ta]", "\"Canadian journal of surgery\"[ta]", "\"Current problems in surgery\"[ta]", "Scandinavian journal of surgery[ta]", "Surgical innovation[ta]", "\"Annals of surgical innovation and research\"[ta]", "Updates in surgery[ta]", "Annals of surgical treatment and research[ta]", "Asian journal of surgery[ta]", "\"Southeast Asian journal of surgery\"[ta]", "Journal of investigative surgery[ta]", "Annals of the Royal College of Surgeons of England[ta]", "\"International surgery\"[ta]", "Indian J Surg[ta]"]
+print(str(len(l)) + " journals searched for")
 searchURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed"
 date = "&datetype=pdat&mindate=2017/03/01&maxdate=2017/03/31"
 count = "&retmax=10000"
@@ -31,7 +33,7 @@ def getKeyWords():
         r = requests.get(searchURL + journal + date + count + output)
         jsonobj = json.loads(r.text)
         idlist += jsonobj["esearchresult"]["idlist"]
-    print(len(idlist))
+    print(str(len(idlist)) + " articles found")
     searchURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed"
     for i in idlist:
         journal = "&id=" + str(i)
@@ -40,13 +42,16 @@ def getKeyWords():
         jsonobj = json.loads(r.text)
         title = jsonobj["result"][str(i)]["title"]
         journalname = jsonobj["result"][str(i)]["source"]
+        journals.add(journalname)
         authors = ""
         for j in jsonobj["result"][str(i)]["authors"]:
             authors += j["name"] + ", "
         authors = authors[:-2]
         authors += ". "
+        #print(journalname)
         pubdate = jsonobj["result"][str(i)]["pubdate"]
         articleinfo = authors + title + " " + journalname + ". " + pubdate + ". " + "https://www.ncbi.nlm.nih.gov/pubmed/?term=" + str(i)
+        titles.append(title)
         for word in title.split():
             word = word.lower()
             word = word.translate(translator)
@@ -59,20 +64,27 @@ def getKeyWords():
                     tempset.add(articleinfo)
                     keyWords[word] = tempset
     keyWords = OrderedDict(sorted(keyWords.items(),key=lambda t: len(t[1]), reverse=True))
+    print(str(len(journals)) + " journals found")
     return keyWords
 
 def storeInDatabase():
     countten = 0
+    counter = 0
     for i in keyWords.keys():
-        if countten == 10:
+        if countten == 30:
             break
         print(i)
-        fb.put('cardiology', str(countten+1), [str(i)] + list(keyWords[i]))
+        for j in titles:
+            if i in j:
+               counter +=1
+        #fb.put(topic, str(countten+1), [str(i)] + list(keyWords[i]))
         print(len(keyWords[i]))
         articles = list(keyWords[i])
         # for j in range(len(articles)):
         #     print(str(j) + "\t" + str(articles[j]))
         countten+=1
+        print(counter)
+        counter = 0
 
 t1 = time.time()
 getKeyWords()
